690397c801202eb79a5d66c5e16fdd5ed735725a
Nikolai Shipilov <nikolai.shipilov@opendaq.com>
2023-11-22 15:51:52 +0100

    Add ControlServer to handle HTTP json-rpc subscribing requests

---
+++ include/streaming_protocol/ControlServer.hpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2022-2023 Blueberry d.o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <memory>
+
+#include <boost/asio/io_context.hpp>
+
+#include "streaming_protocol/Logging.hpp"
+#include "streaming_protocol/Types.h"
+
+namespace daq::streaming_protocol {
+
+class listener;
+
+class ControlServer
+{
+public:
+    /// \addtogroup producer
+    /// This class is used by producer to run the control service for subscribing/unsubscribing signals
+
+    /// Executes subscribe/unsubscribe command
+    /// \param streamId A unique ID identifying the stream instance
+    /// \param command Command received from client, might be 'subscribe' or 'unsubscribe'.
+    /// \param signalIds List of signals to be subscribed/unsubscribed.
+    /// \param errorMessage Filled with error message to be responded to client if command failed
+    /// \return negative status code if command failed or '0' if command succeeded
+    using CommandCb = std::function < int (
+        const std::string& streamId,
+        const std::string& command,
+        const SignalIds& signalIds,
+        std::string& errorMessage
+    ) >;
+
+    ControlServer(boost::asio::io_context& ioc, uint16_t port, CommandCb commandCb, LogCallback logCb);
+    ~ControlServer();
+
+    /// Starts control HTTP server on port
+    void start();
+    void stop();
+
+    uint16_t getPort();
+
+private:
+    boost::asio::io_context& m_ioc;
+    std::shared_ptr<listener> m_listener;
+    uint16_t m_port;
+    CommandCb m_commandCb;
+    LogCallback logCallback;
+};
+
+}
--- test/ControlTest.cpp
+++ test/ControlTest.cpp
@@ -23,43 +23,31 @@
 #include "streaming_protocol/Logging.hpp"
 
 #include "../lib/Controller.hpp"
+#include "streaming_protocol/ControlServer.hpp"
+
+#include <thread>
+#include <future>
 
 namespace daq::streaming_protocol {
     static const std::string streamId = "theId";;
     static const std::string signalId = "a signal id";
     static const std::string address = "localhost";
-    static const unsigned int port = 6000;
+    static const uint16_t port = 7438;
     static const std::string target = "/";
     static const unsigned int httpVersion = 11;
-    static boost::asio::io_context ioc;
     static LogCallback logCallback = daq::streaming_protocol::Logging::logCallback();
+    static const std::chrono::seconds timeout = std::chrono::seconds(2);
 
-    TEST(ControlTest, empty_stream_id)
+    TEST(ControlClientTest, empty_stream_id)
     {
+        boost::asio::io_context ioc;
         // excepction because of empty stream id
         EXPECT_THROW(Controller controller(ioc, "", address, std::to_string(port), target, httpVersion, logCallback), std::runtime_error);
     }
 
-//    TEST(ControlTest, empty_signal_id_list_subscribe_unsubscribe)
-//    {
-//        Controller controller(ioc, streamId, address, std::to_string(port), target, httpVersion);
-//        SignalIds signalIds;
-
-//        boost::system::error_code ec;
-//        unsigned int count = 0;
-//        auto resultCb = [&](const boost::system::error_code& cbEc) {
-//            ec = cbEc;
-//            ++count;
-//        };
-//        controller.asyncSubscribe(signalIds, resultCb);
-//        controller.asyncUnsubscribe(signalIds, resultCb);
-
-//        ioc.run();
-//        ASSERT_EQ(count, 2);
-//    }
-
-    TEST(ControlTest, signal_id_list_subscribe_unsubscribe)
+    TEST(ControlClientTest, signal_id_list_subscribe_unsubscribe)
     {
+        boost::asio::io_context ioc;
         Controller controller(ioc, streamId, address, std::to_string(port), target, httpVersion, logCallback);
         SignalIds signalIds;
 
@@ -78,5 +66,115 @@ namespace daq::streaming_protocol {
         ASSERT_EQ(count, 2);
     }
 
+    TEST(ControlServerClientTest, subscribe_unsubscribe_success)
+    {
+        boost::asio::io_context ioc;
+        Controller controller(ioc, streamId, address, std::to_string(port), target, httpVersion, logCallback);
+        SignalIds signalIds;
+
+        std::string recvStreamId;
+        std::string recvCommand;
+        SignalIds recvSignalIds;
+        std::promise<void> serverPromise;
+        ControlServer::CommandCb commandCb = [&](
+            const std::string& _streamId,
+            const std::string& _command,
+            const SignalIds& _signalIds,
+            std::string& /*_errorMessage*/
+        )
+        {
+            recvStreamId = _streamId;
+            recvCommand = _command;
+            recvSignalIds = _signalIds;
+            serverPromise.set_value();
+            return 0;
+        };
+        ControlServer server(ioc, port, commandCb, logCallback);
+
+        boost::system::error_code ec;
+        unsigned int count = 0;
+        std::promise<void> clientPromise;
+        auto resultCb = [&](const boost::system::error_code& cbEc) {
+            ec = cbEc;
+            ++count;
+            clientPromise.set_value();
+        };
+
+        signalIds.push_back(signalId);
+        server.start();
+        auto workerThread = std::thread([&]() { ioc.run(); });
+
+        std::future<void> serverFuture = serverPromise.get_future();
+        std::future<void> clientFuture = clientPromise.get_future();
+        controller.asyncSubscribe(signalIds, resultCb);
+        EXPECT_EQ(serverFuture.wait_for(timeout), std::future_status::ready);
+        EXPECT_EQ(recvStreamId, streamId);
+        EXPECT_EQ(recvCommand, "subscribe");
+        EXPECT_EQ(recvSignalIds, signalIds);
+        EXPECT_EQ(clientFuture.wait_for(timeout), std::future_status::ready);
+        EXPECT_EQ(count, 1);
+        EXPECT_FALSE(ec.failed());
+
+        serverPromise = std::promise<void>();
+        clientPromise = std::promise<void>();
+        serverFuture = serverPromise.get_future();
+        clientFuture = clientPromise.get_future();
+        controller.asyncUnsubscribe(signalIds, resultCb);
+        EXPECT_EQ(serverFuture.wait_for(timeout), std::future_status::ready);
+        EXPECT_EQ(recvStreamId, streamId);
+        EXPECT_EQ(recvCommand, "unsubscribe");
+        EXPECT_EQ(recvSignalIds, signalIds);
+        EXPECT_EQ(clientFuture.wait_for(timeout), std::future_status::ready);
+        EXPECT_EQ(count, 2);
+        EXPECT_FALSE(ec.failed());
+
+        ioc.stop();
+        workerThread.join();
+    }
+
+    TEST(ControlServerClientTest, server_error)
+    {
+        boost::asio::io_context ioc;
+        Controller controller(ioc, streamId, address, std::to_string(port), target, httpVersion, logCallback);
+        SignalIds signalIds;
+
+        std::promise<void> serverPromise;
+        ControlServer::CommandCb commandCb = [&](
+            const std::string& /*_streamId*/,
+            const std::string& /*_command*/,
+            const SignalIds& /*_signalIds*/,
+            std::string& _errorMessage
+        )
+        {
+            _errorMessage = "server error";
+            serverPromise.set_value();
+            return -1;
+        };
+        ControlServer server(ioc, port, commandCb, logCallback);
+
+        boost::system::error_code ec;
+        unsigned int count = 0;
+        std::promise<void> clientPromise;
+        auto resultCb = [&](const boost::system::error_code& cbEc) {
+            ec = cbEc;
+            ++count;
+            clientPromise.set_value();
+        };
+
+        signalIds.push_back(signalId);
+        server.start();
+        auto workerThread = std::thread([&]() { ioc.run(); });
+
+        std::future<void> serverFuture = serverPromise.get_future();
+        std::future<void> clientFuture = clientPromise.get_future();
+        controller.asyncSubscribe(signalIds, resultCb);
+        EXPECT_EQ(serverFuture.wait_for(timeout), std::future_status::ready);
+        EXPECT_EQ(clientFuture.wait_for(timeout), std::future_status::ready);
+        EXPECT_EQ(count, 1);
+        EXPECT_FALSE(ec.failed());
+
+        ioc.stop();
+        workerThread.join();
+    }
 
 }
