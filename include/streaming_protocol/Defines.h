88360845aa42fed08b0f5f9d04964b54d2b5510d
Nikolai Shipilov <nikolai.shipilov@opendaq.com>
2025-04-15 14:39:39 +0200

    Set version 1.2.3

--- include/streaming_protocol/Defines.h
+++ include/streaming_protocol/Defines.h
@@ -129,6 +129,6 @@ static const char META_RANGE[] = "range";
 static const char META_LOW[] = "low";
 static const char META_HIGH[] = "high";
 
-static const char OPENDAQ_LT_STREAM_VERSION[] = "1.2.2";
+static const char OPENDAQ_LT_STREAM_VERSION[] = "1.2.3";
 
 }
